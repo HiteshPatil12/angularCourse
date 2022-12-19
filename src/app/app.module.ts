6f304691c5e71ca0cb02f198fa41acf9a0ec876a
Hitesh <hiteshpatil394@gmail.com>
2022-12-19 07:26:40 +0000

    new component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { TrialComponent } from './trial/trial.component';
 import { AppComponent } from './app.component';
 import { StockItemComponent } from './stock/stock-item/stock-item.component';
+import { PracticeComponent } from './practice/practice.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TrialComponent,
     StockItemComponent,
+    PracticeComponent,
   ],
   imports: [
     BrowserModule
