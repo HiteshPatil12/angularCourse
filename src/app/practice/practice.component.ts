8c7a9b0e73a9a047f29c615226ad26d0472afefc
Hitesh <hiteshpatil394@gmail.com>
2022-12-19 10:02:56 +0000

    stock component added

--- src/app/practice/practice.component.ts
+++ src/app/practice/practice.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./practice.component.css']
 })
 export class PracticeComponent {
-
+  age = 100;
 }
--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./stock-item.component.css']
 })
 export class StockItemComponent {
+  name = "";
+  code = "";
+  price = 0;
+  previousPrice = 0;
+  positiveChange = true;
+  Favourite = true;
 
+  constructor(){
+    this.name = 'Tata Consultancy Services';
+    this.code = 'TCS';
+    this.price = 3500;
+    this.previousPrice = 3400;
+    this.positiveChange = this.price >= this.previousPrice;
+    this.Favourite = false;
+  }
+  toggleFavourite(){
+    this.Favourite = !this.Favourite;
+  }
 }
